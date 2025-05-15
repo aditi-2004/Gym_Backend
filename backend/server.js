359e135c91a2ff61c87522ead0038d88b3f64eb4
ADITI GOEL <115215112+aditi-2004@users.noreply.github.com>
2025-05-15 15:31:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -112,7 +112,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
   .catch(err => console.error("MongoDB connection error:", err));
 
 // app.use(cors());
-const cors = require('cors');
+
 app.use(cors({
   origin: 'https://bilvine-empire-frnt.onrender.com', // or '*'
   credentials: true
